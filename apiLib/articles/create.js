9b8ab63727cb64626e2d92b2a73fae0fdfa34643
Jesper Hägglund <jesper.hagglund@trmedia.se>
2020-02-19 18:59:50 -0300

    create article

---
+++ apiLib/articles/create.js
@@ -0,0 +1,25 @@
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: process.env.REGION,
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY
+});
+// Create DynamoDB document client
+const docClient = new AWS.DynamoDB.DocumentClient({
+  apiVersion: "2012-08-10",
+  convertEmptyValues: true
+});
+
+export default async (req, res) => {
+  const { data } = req.body;
+  const params = {
+    TableName: "ubit-articles",
+    Item: data
+  };
+  const resData = await docClient.put(params).promise();
+  res.setHeader("Content-Type", "application/json");
+
+  res.statusCode = 200;
+  res.end(JSON.stringify({ resData }));
+};
