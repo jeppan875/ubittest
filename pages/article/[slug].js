1bedde000f2db43e6079183b74c85ac5102af98d
Jesper Hägglund <jesper.hagglund@trmedia.se>
2020-02-21 12:36:45 -0300

    fetch on server

--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -1,9 +1,19 @@
 import React from "react";
 import ArticlePage from "containers/ArticlePage";
+import { setArticle } from "containers/ArticlePage//actions";
+import fetch from "isomorphic-unfetch";
+const devUrl = "http://localhost:8082";
 const Article = () => {
   return <ArticlePage />;
 };
 
-Article.getInitialProps = async ({ store, req }) => {};
+Article.getInitialProps = async ({ store, req, query }) => {
+  if (req) {
+    const { slug } = query;
+    const res = await fetch(`${devUrl}/api/articles?slug=${slug}`);
+    const data = await res.json();
+    store.dispatch(setArticle({ data, slug }));
+  }
+};
 
 export default Article;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import HomePage from "containers/HomePage";
+import { setArticles } from "containers/HomePage//actions";
+import fetch from "isomorphic-unfetch";
+const devUrl = "http://localhost:8082";
 const Home = () => {
   return <HomePage />;
 };
 
-Home.getInitialProps = async ({ store, req }) => {};
+Home.getInitialProps = async ({ store, req }) => {
+  if (req) {
+    const res = await fetch(`${devUrl}/api/articles`);
+    const data = await res.json();
+    store.dispatch(setArticles({ data }));
+  }
+};
 
 export default Home;
