62238455fc9faf9343553316ad888e9ad882b62e
Jesper Hägglund <jesper.hagglund@trmedia.se>
2020-02-21 11:26:11 -0300

    restyle front articles

--- components/ArticleItem/index.js
+++ components/ArticleItem/index.js
@@ -8,6 +8,12 @@ const Root = styled.div`
   cursor: pointer;
 `;
 
+const ContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
 const SimpleLink = styled.a`
   text-decoration: none;
   &:focus {
@@ -22,67 +28,27 @@ const Image = styled.img`
   ${media.tablet`
     max-height: 300px;
   `}
-  transform: scale(1.01);
+  transition: all .2s ease-in-out;
+  &:hover {
+    transform: scale(1.02);
+  }
 `;
 
 const Content = styled.div`
-  position: absolute;
-  bottom: 0;
-  color: white;
+  align-self: center;
+  margin-top: -10px;
+  display: flex;
+  justify-content: center;
+  width: 90%;
+  background: ${colors.greys[2]};
+  color: black;
   z-index: 3;
   padding: 10px;
   overflow: hidden;
 `;
 
-const Relative = styled.div`
-  position: relative;
-  transition: all 0.2s ease-in-out;
-  &:hover {
-    transform: scale(1.03);
-  }
-`;
-
 const Title = styled.h3`
-  margin: 0 0 10px 0;
-`;
-
-const Fade = styled.div`
-  content: "";
-  box-sizing: border-box;
-  position: absolute;
-  z-index: 2;
-  bottom: 0;
-  left: 0;
-  pointer-events: none;
-  background-image: linear-gradient(
-    to bottom,
-    rgba(0, 0, 0, 0),
-    rgba(0, 0, 0, 1) 100%
-  );
-  width: 100%;
-  height: 6em;
-`;
-
-const FadeText = styled.div`
-  content: "";
-  box-sizing: border-box;
-  position: absolute;
-  z-index: 3;
-  bottom: 0;
-  left: 0;
-  pointer-events: none;
-  background-image: linear-gradient(
-    to bottom,
-    rgba(0, 0, 0, 0),
-    rgba(0, 0, 0, 1) 100%
-  );
-  width: 100%;
-  height: 1em;
-`;
-
-const Text = styled.p`
-  max-height: 25px;
-  word-break: break-all;
+  margin: 0;
 `;
 
 export default ({ article }) => {
@@ -91,15 +57,12 @@ export default ({ article }) => {
     <Root>
       <Link href={`/article/[slug]`} as={`/article/${slug}`} passHref>
         <SimpleLink>
-          <Relative>
+          <ContentWrapper>
             <Image src={imgurl} />
             <Content>
               <Title>{title}</Title>
-              <Text>{text}</Text>
-              <FadeText />
             </Content>
-            <Fade />
-          </Relative>
+          </ContentWrapper>
         </SimpleLink>
       </Link>
     </Root>
